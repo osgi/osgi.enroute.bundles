4f31132f5822702c0bd8023ae7bd1b8e941da416
daghan <daghanacay@gmail.com>
2015-07-05 21:30:11 +1000

    Initial Commit
    Hi there, this is my initial contribution to enRoute. In this commit I
    have fixed the code to export gogo command properly. The aim of this
    commit is to understand the rules for contributing to enRoute. Please
    let me know if you have a guide if not please check my questions below.
    My second objective is commit Unit test classes. However before I
    proceed here are some questions:
    
    1- do you have a formatting file for Eclipse, I do not wish to change
    the sourcce formatting through commits?
    2- can I add bnd.bndrun to create debug runtime?
    3- can I modify files such as .gitignore?
    
    Signed-off-by: daghan <daghanacay@gmail.com>

--- osgi.enroute.authenticator.simple.provider/src/osgi/enroute/authenticator/simple/provider/UserAdminAuthenticator.java
+++ osgi.enroute.authenticator.simple.provider/src/osgi/enroute/authenticator/simple/provider/UserAdminAuthenticator.java
@@ -27,207 +27,212 @@ import aQute.lib.base64.Base64;
 import aQute.lib.hex.Hex;
 
 @Component(property = {
-		Debug.COMMAND_FUNCTION + "=hash", Debug.COMMAND_FUNCTION + "=passwd", Debug.COMMAND_FUNCTION + "=adduser",
-		Debug.COMMAND_FUNCTION + "=rmrole", Debug.COMMAND_FUNCTION + "=role"
+        Debug.COMMAND_SCOPE + "=auth", Debug.COMMAND_FUNCTION + "=hash", Debug.COMMAND_FUNCTION + "=passwd", Debug.COMMAND_FUNCTION + "=adduser",
+        Debug.COMMAND_FUNCTION + "=rmrole", Debug.COMMAND_FUNCTION + "=role"
 })
 public class UserAdminAuthenticator implements Authenticator {
-	private static final Pattern	AUTHORIZATION_P	= Pattern.compile("Basic\\s+(?<base64>[A-Za-z0-9+/]{3,}={0,2})");
-	private static final Pattern	IDPW_P			= Pattern.compile("(?<id>[^:]+):(?<pw>.*)");
-	private UserAdmin				userAdmin;
-	private Logger					log;
+    private static final Pattern	AUTHORIZATION_P	= Pattern.compile("Basic\\s+(?<base64>[A-Za-z0-9+/]{3,}={0,2})");
+    private static final Pattern	IDPW_P			= Pattern.compile("(?<id>[^:]+):(?<pw>.*)");
+    private UserAdmin				userAdmin;
+    private Logger					log;
 
-	private byte[]					salt;
-	private Algorithm				algorithm;
-	private int						iterations;
-	private String					root;
+    private byte[]					salt;
+    private Algorithm				algorithm;
+    private int						iterations;
+    private String					root;
 
-	@Activate
-	void activate(Map<String,Object> args) {
-		Config config = Configurable.createConfigurable(Config.class, args);
+    @Activate
+    void activate(final Map<String,Object> args) {
+        final Config config = Configurable.createConfigurable(Config.class, args);
 
-		salt = config.salt();
-		if (salt == null || salt.length == 0) {
-			salt = new byte[] {
-					0x2f, 0x68, (byte) 0xcb, 0x75, 0x6c, (byte) 0xf1, 0x74, (byte) 0x84, 0x2a, (byte) 0xef
-			};
-		}
+        this.salt = config.salt();
+        if (this.salt == null || this.salt.length == 0) {
+            this.salt = new byte[] {
+                    0x2f, 0x68, (byte) 0xcb, 0x75, 0x6c, (byte) 0xf1, 0x74, (byte) 0x84, 0x2a, (byte) 0xef
+            };
+        }
 
-		algorithm = config.algorithm();
-		if (algorithm == null) {
-			algorithm = Algorithm.PBKDF2WithHmacSHA1;
-		}
+        this.algorithm = config.algorithm();
+        if (this.algorithm == null) {
+            this.algorithm = Algorithm.PBKDF2WithHmacSHA1;
+        }
 
-		iterations = config.iterations();
-		if (iterations < 100)
-			iterations = 997;
+        this.iterations = config.iterations();
+        if (this.iterations < 100) {
+            this.iterations = 997;
+        }
 
-		root = config._root();
-		if (root != null && root.trim().isEmpty())
-			root = null;
-	}
+        this.root = config._root();
+        if (this.root != null && this.root.trim().isEmpty()) {
+            this.root = null;
+        }
+    }
 
-	@Override
-	public String authenticate(Map<String,Object> arguments, String... sources) throws Exception {
+    @Override
+    public String authenticate(final Map<String,Object> arguments, final String... sources) throws Exception {
 
-		for (String source : sources) {
+        for (final String source : sources) {
 
-			if (Authenticator.BASIC_SOURCE_PASSWORD.equals(source)) {
-				String id = (String) arguments.get(Authenticator.BASIC_SOURCE_USERID);
-				String pw = (String) arguments.get(Authenticator.BASIC_SOURCE_PASSWORD);
-				if (id != null && pw != null)
-					return verify(id, pw);
+            if (Authenticator.BASIC_SOURCE_PASSWORD.equals(source)) {
+                final String id = (String) arguments.get(Authenticator.BASIC_SOURCE_USERID);
+                final String pw = (String) arguments.get(Authenticator.BASIC_SOURCE_PASSWORD);
+                if (id != null && pw != null) {
+                    return verify(id, pw);
+                }
 
-				log.info("BASIC_SOURCE_PASSWORD specified but no userid/password found in arguments");
-			}
+                this.log.info("BASIC_SOURCE_PASSWORD specified but no userid/password found in arguments");
+            }
 
-			if (Authenticator.SERVLET_SOURCE.equals(source)) {
-				String uri = (String) arguments.get(Authenticator.SERVLET_SOURCE_METHOD);
-				if (uri.startsWith("https:")) {
-					TreeMap<String,Object> map = new TreeMap<>(String.CASE_INSENSITIVE_ORDER);
-					map.putAll(arguments);
+            if (Authenticator.SERVLET_SOURCE.equals(source)) {
+                final String uri = (String) arguments.get(Authenticator.SERVLET_SOURCE_METHOD);
+                if (uri.startsWith("https:")) {
+                    final TreeMap<String,Object> map = new TreeMap<>(String.CASE_INSENSITIVE_ORDER);
+                    map.putAll(arguments);
 
-					String auth = (String) map.get("Authorization");
-					if (auth != null) {
-						Matcher m = AUTHORIZATION_P.matcher(auth);
+                    final String auth = (String) map.get("Authorization");
+                    if (auth != null) {
+                        Matcher m = AUTHORIZATION_P.matcher(auth);
 
-						if (m.find()) {
-							String base64 = m.group("base64");
-							byte[] bytes = Base64.decodeBase64(base64);
-							String pwId = new String(bytes, "UTF-8");
-							m = IDPW_P.matcher(pwId);
-							if (m.matches()) {
-								String id = m.group("id");
-								String pw = m.group("pw");
-								return verify(id, pw);
-							}
-						}
+                        if (m.find()) {
+                            final String base64 = m.group("base64");
+                            final byte[] bytes = Base64.decodeBase64(base64);
+                            final String pwId = new String(bytes, "UTF-8");
+                            m = IDPW_P.matcher(pwId);
+                            if (m.matches()) {
+                                final String id = m.group("id");
+                                final String pw = m.group("pw");
+                                return verify(id, pw);
+                            }
+                        }
 
-						// assume it is not Basic but something else
+                        // assume it is not Basic but something else
 
-					} else {
-						log.warn("Servlet authentication requires an Authorization header");
-					}
-				} else {
-					log.warn("Servlet authentication requires https {}", uri);
-				}
-			}
-		}
-		return null;
-	}
+                    } else {
+                        this.log.warn("Servlet authentication requires an Authorization header");
+                    }
+                } else {
+                    this.log.warn("Servlet authentication requires https {}", uri);
+                }
+            }
+        }
+        return null;
+    }
 
-	@Override
-	public boolean forget(String userid) throws Exception {
-		return false;
-	}
+    @Override
+    public boolean forget(final String userid) throws Exception {
+        return false;
+    }
 
-	private String verify(String id, String pw) throws Exception {
-		Role role = userAdmin.getRole(id);
-		if (role == null) {
-			log.info("Failed login attempt for %s: no such user", id);
-			return null;
-		}
+    private String verify(final String id, final String pw) throws Exception {
+        final Role role = this.userAdmin.getRole(id);
+        if (role == null) {
+            this.log.info("Failed login attempt for %s: no such user", id);
+            return null;
+        }
 
-		if (!(role instanceof User)) {
-			log.info("Failed login attempt for %s: id is not a user name but %s", id, role);
-			return null;
-		}
+        if (!(role instanceof User)) {
+            this.log.info("Failed login attempt for %s: id is not a user name but %s", id, role);
+            return null;
+        }
 
-		User user = (User) role;
+        final User user = (User) role;
 
-		String hash = hash(pw);
-		if (user.hasCredential(algorithm.toString(), hash))
-			return id;
+        final String hash = hash(pw);
+        if (user.hasCredential(this.algorithm.toString(), hash)) {
+            return id;
+        }
 
-		if (root != null && root.equals(hash)) {
-			log.info("Root login by %s", id);
-			return id;
-		}
+        if (this.root != null && this.root.equals(hash)) {
+            this.log.info("Root login by %s", id);
+            return id;
+        }
 
-		log.info("Failed login attempt for %s: invalid password", id);
-		return null;
-	}
+        this.log.info("Failed login attempt for %s: invalid password", id);
+        return null;
+    }
 
-	public String hash(String password) throws Exception {
-		switch (algorithm) {
-			default :
-			case PBKDF2WithHmacSHA1 :
-				byte[] hash = pbkdf2(password.toCharArray(), salt, iterations, 24);
-				return Hex.toHexString(hash);
-		}
-	}
+    public String hash(final String password) throws Exception {
+        switch (this.algorithm) {
+        default :
+        case PBKDF2WithHmacSHA1 :
+            final byte[] hash = pbkdf2(password.toCharArray(), this.salt, this.iterations, 24);
+            return Hex.toHexString(hash);
+        }
+    }
 
-	byte[] pbkdf2(char[] password, byte[] salt, int iterations, int bytes) throws Exception {
-		PBEKeySpec spec = new PBEKeySpec(password, salt, iterations, bytes * 8);
-		SecretKeyFactory skf = SecretKeyFactory.getInstance(algorithm.toString());
-		return skf.generateSecret(spec).getEncoded();
-	}
+    byte[] pbkdf2(final char[] password, final byte[] salt, final int iterations, final int bytes) throws Exception {
+        final PBEKeySpec spec = new PBEKeySpec(password, salt, iterations, bytes * 8);
+        final SecretKeyFactory skf = SecretKeyFactory.getInstance(this.algorithm.toString());
+        return skf.generateSecret(spec).getEncoded();
+    }
 
-	public Role adduser(String id) {
-		// TODO validate id
-		return userAdmin.createRole(id, Role.USER);
-	}
+    public Role adduser(final String id) {
+        // TODO validate id
+        return this.userAdmin.createRole(id, Role.USER);
+    }
 
-	public List<Role> role(String... filter) throws InvalidSyntaxException {
-		List<Role> roles = new ArrayList<>();
+    public List<Role> role(final String... filter) throws InvalidSyntaxException {
+        final List<Role> roles = new ArrayList<>();
 
-		if (filter.length == 0) {
-			getRoles(roles, null);
-		} else {
+        if (filter.length == 0) {
+            getRoles(roles, null);
+        } else {
 
-			for (String f : filter) {
-				if (f.startsWith("(") && f.endsWith(")")) {
-					getRoles(roles, f);
-				} else {
-					Role r = userAdmin.getRole(f);
-					if (f != null)
-						roles.add(r);
-				}
-			}
-		}
-		return roles;
-	}
+            for (final String f : filter) {
+                if (f.startsWith("(") && f.endsWith(")")) {
+                    getRoles(roles, f);
+                } else {
+                    final Role r = this.userAdmin.getRole(f);
+                    if (f != null) {
+                        roles.add(r);
+                    }
+                }
+            }
+        }
+        return roles;
+    }
 
-	private void getRoles(List<Role> roles, String filter) throws InvalidSyntaxException {
-		Role[] rs = userAdmin.getRoles(filter);
-		if (rs != null) {
-			for (Role role : rs) {
-				roles.add(role);
-			}
-		}
-	}
+    private void getRoles(final List<Role> roles, final String filter) throws InvalidSyntaxException {
+        final Role[] rs = this.userAdmin.getRoles(filter);
+        if (rs != null) {
+            for (final Role role : rs) {
+                roles.add(role);
+            }
+        }
+    }
 
-	public int rmrole(String... id) {
-		int n = 0;
-		for (String i : id) {
-			userAdmin.removeRole(i);
-			n++;
-		}
-		return n;
-	}
+    public int rmrole(final String... id) {
+        int n = 0;
+        for (final String i : id) {
+            this.userAdmin.removeRole(i);
+            n++;
+        }
+        return n;
+    }
 
-	@SuppressWarnings("unchecked")
-	public void passwd(String id, String pw) throws Exception {
-		Role role = userAdmin.getRole(id);
+    @SuppressWarnings("unchecked")
+    public void passwd(final String id, final String pw) throws Exception {
+        Role role = this.userAdmin.getRole(id);
 
-		if (role == null) {
-			role = userAdmin.createRole(id, Role.USER);
-		} else if (!(role instanceof User)) {
-			System.err.println("Not a user role, but " + role);
-		}
+        if (role == null) {
+            role = this.userAdmin.createRole(id, Role.USER);
+        } else if (!(role instanceof User)) {
+            System.err.println("Not a user role, but " + role);
+        }
 
-		User user = (User) role;
+        final User user = (User) role;
 
-		user.getCredentials().put(algorithm.toString(), hash(pw));
-	}
+        user.getCredentials().put(this.algorithm.toString(), hash(pw));
+    }
 
-	@Reference
-	void setUA(UserAdmin userAdmin) {
-		this.userAdmin = userAdmin;
-	}
+    @Reference
+    void setUA(final UserAdmin userAdmin) {
+        this.userAdmin = userAdmin;
+    }
 
-	@Reference
-	void setLog(Logger logger) {
-		this.log = logger;
-	}
+    @Reference
+    void setLog(final Logger logger) {
+        this.log = logger;
+    }
 
 }
